66f25c7eab9849a26322cb01beebcf609b974590
Spencer Owen <spencerowen@Spencers-MacBook-Pro.local>
2013-08-21 20:29:23 -0600

    Adds web folder from chris

---
+++ web/README.txt
@@ -0,0 +1,4 @@
+Spencer, 
+
+The files in this directory are a copy of the files that chris put on acintranet.wpengine.com
+They are the oauth redirect page for salesforce 
---
+++ web/resttest/config.php
@@ -0,0 +1,7 @@
+<?php
+define("CLIENT_ID2", "foo");
+define("CLIENT_ID", "bar");
+define("CLIENT_SECRET", "foobar");
+define("REDIRECT_URI", "https://acintranet.wpengine.com/resttest/oauth_callback.php");
+define("LOGIN_URI", "https://login.salesforce.com");
+?>
